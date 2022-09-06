9b014f9be6b3015cb507abd46f1264412d912686
Nick Hanigan <13477700+nick-wireless@users.noreply.github.com>
2022-09-06 23:04:34 +0930

    Bug fix
    Removing transition items.

--- components/ArtboardSunAndClouds.vue
+++ components/ArtboardSunAndClouds.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative h-80 ">
     <div class="absolute top-0 -left-32 bg-white w-[400px] h-96 rounded-full mixed-blend-multiply filter blur-xl
-        opacity-0">
+          opacity-0">
     </div>
     <div
       class="absolute top-0 -top-24 bg-gray-600 w-60 h-60 rounded-full mixed-blend-multiply filter blur-lg opacity-0">
@@ -9,28 +9,3 @@
   </div>
 </template>
 
-<script>
-export default {
-  transition: 'move'
-}
-</script>
-
-<style>
-.move {
-  animation: slow-move 4s infinite;
-}
-
-@keyframes slow-move {
-  0%: {
-    transform: scale(1);
-  }
-
-  50% {
-    transform: scale(2);
-  }
-
-  100% {
-    transform: scale(1)
-  }
-}
-</style>
